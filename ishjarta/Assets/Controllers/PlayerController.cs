aadcfe459dd57f47dbe56df5e2a147e05e2aa2a3
Danijel Balog <55655771+FragonGamer@users.noreply.github.com>
2022-01-08 19:12:11 +0100

    Changed name of  PlayerManager to PlayerController

---
+++ ishjarta/Assets/Controllers/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Player player;
+    private void Awake()
+    {
+        player = (Player)GameObject.FindWithTag("Player").GetComponent(typeof(Player));
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            player.Attack(Input.mousePosition);
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Debug.Log("Dropped Item");
+            if (player.inventory.GetActiveItem() != null)
+            {
+                ActiveItem activeItem = player.inventory.GetActiveItem();
+                player.inventory.DropItem(activeItem);
+            }
+        }
+    }
+
+}
--- ishjarta/Assets/Scripts/Enemy.cs
+++ ishjarta/Assets/Scripts/Enemy.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : Entity
 {
     [SerializeField] Animator animator;
     [SerializeField] int dropRate, spottingRange;
     [SerializeField] bool hasSpottedPlayer, isInRange;
-    
+    [SerializeField] Transform target;
+    [SerializeField] NavMeshAgent agent;
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        target = GetComponent<Transform>();
+    }
     public override void Attack(Vector2 vector)
     {
         throw new System.NotImplementedException();
@@ -18,5 +25,10 @@ public class Enemy : Entity
     {
         animator.SetBool("isDead", true);
     }
-    
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, spottingRange);
+    }
 }
