0734f35c34ab098e0ab2f5e7a7143a8b97fca2db
IanDO <Ian.dworzak@gmail.com>
2022-01-09 19:23:02 +0100

    worked on active items and the ig overlay
    - added scripts for active items
    - added a script for an item effect
    - modified inventory
    - created a canvas prefab with the health bar overlay

--- ishjarta/Assets/Scripts/Items/ActiveItem.cs
+++ ishjarta/Assets/Scripts/Items/ActiveItem.cs
@@ -5,5 +5,18 @@ using UnityEngine;
 [CreateAssetMenu]
 public class ActiveItem : Item
 {
+    public float cooldownTime;
+    public float activeTime;
 
+    // Line 11 ist für Testzwecke, später löschen (einfach mich anheulen - Ian)
+    public float dashVelocity;
+
+    public virtual void Activate(GameObject parent) 
+    {
+        // Alles in dieser Klasse ist für Testzwecke, später löschen (einfach mich anheulen - Ian)
+        PlayerMovement movement = parent.GetComponent<PlayerMovement>();
+        Rigidbody2D rigidBody = parent.GetComponent<Rigidbody2D>();
+
+        rigidBody.velocity = movement.movement.normalized * dashVelocity;
+    }
 }
