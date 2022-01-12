1bbdeda0f4f3d79d6889fc924670c2ca479ebe02
Danijel Balog <thegamerdb1@gmail.com>
2022-01-12 12:00:43 +0100

    edited arrow collison

--- ishjarta/Assets/Animations/DestroyOnExit.cs
+++ ishjarta/Assets/Animations/DestroyOnExit.cs
@@ -1,3 +1,4 @@
+using Pathfinding;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,10 @@ public class DestroyOnExit : StateMachineBehaviour
 {
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        GameObject go = animator.gameObject;
+        go.tag = "Untagged";
+        AIPath pathfinding = go.GetComponent<AIPath>();
+        pathfinding.enabled = false;
         Destroy(animator.gameObject, stateInfo.length+0.5f);
     }
 }
--- ishjarta/Assets/Scripts/Arrow.cs
+++ ishjarta/Assets/Scripts/Arrow.cs
@@ -8,12 +8,13 @@ public class Arrow : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if(collider.gameObject.tag == "Enemy")
+        if(collider.gameObject.tag == "Enemy"&&collider.GetType() == typeof(BoxCollider2D))
         {
             collider.gameObject.GetComponent<Enemy>().ReceiveDamage(DealingDammage);
             Destroy(this.gameObject);
         }
-        else if (collider.gameObject.tag == "Walls" || collider.gameObject.tag == "Obstacle")
+        // 3 is obstacle layer and 6 is wall layer
+        else if (collider.gameObject.layer == 3  || collider.gameObject.layer == 6)
         {
             Destroy(this.gameObject);
         }
