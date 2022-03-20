111ae06044880b3dc6d0103c5399591e00672ecd
Danijel Balog <55655771+FragonGamer@users.noreply.github.com>
2022-03-20 19:35:14 +0100

    1

--- ishjarta/Assets/Scripts/DoorTrigger.cs
+++ ishjarta/Assets/Scripts/DoorTrigger.cs
@@ -5,13 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class DoorTrigger : MonoBehaviour
 {
-    public string levelIndex;
     public string nextSceneString;
     public bool doorIsOpen;
 
     public void OnTriggerEnter2D(Collider2D collider)
     {
         if (doorIsOpen && collider.tag == "Player")
-            SceneManager.LoadScene($"Scenes/{levelIndex}/{nextSceneString}");
+            SceneManager.LoadScene($"Scenes/{nextSceneString}");
     }
 }
