ef1ef51aeecf87e66b24e4c8c4e09c9355662fe8
FelixHint <coffewizzard6@gmail.com>
2022-10-10 18:14:05 +0200

    added new scrip for RangedSlime
    I just doesn't work.
    I don't know why

---
+++ ishjarta/Assets/Scripts/UnitSound.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitSound : MonoBehaviour
+{
+    AudioSource audioSource;
+    public AudioClip[] sounds = new AudioClip[1];
+
+    private NavMeshAgent nma;
+
+    bool walkingSoundRunning = false;
+
+    IEnumerator PlayWalkingSound(){
+        audioSource.clip = sounds[0];
+        audioSource.Play();
+        walkingSoundRunning = true;
+        yield return new WaitForSeconds(.7f);
+    }
+
+    void Start(){
+        nma = GetComponent<NavMeshAgent>();
+    }
+
+    void FixedUpdate(){
+        if (walkingSoundRunning == false){
+            StartCoroutine(PlayWalkingSound());
+            walkingSoundRunning = false;
+        }
+    }
+    
+}
