e9831e51862fe28af1739700afaffda1bc3e65a3
sandin <sandin-habibovic@hotmail.com>
2022-11-13 19:38:01 +0100

    created build-docu action

---
+++ .github/workflows/docs/testCounter.cs
@@ -0,0 +1,18 @@
+/// <summary>
+/// This class manages a counter
+/// </summary>
+public class TestCounter
+{
+    /// <summary>
+    /// This field specifies a counter
+    /// </summary>
+    public int Counter { get; set; }
+
+    /// <summary>
+    /// This method increments the counter
+    /// </summary>
+    public void increment()
+    {
+        Counter++;
+    }
+}
